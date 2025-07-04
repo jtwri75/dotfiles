4dcf56f1d38f1bde751b9a30b7d34f336eaf90e0
jtwri75 <jtwri75@gmail.com>
2025-07-04 18:16:07 +0530

    dotfiles: user: starship: set character symbol

--- user/programs/starship.nix
+++ user/programs/starship.nix
@@ -3,5 +3,17 @@
   pkgs,
   ...
 }: {
-  programs.starship.enable = true;
+  programs.starship = {
+    enable = true;
+    settings = {
+      character = {
+        success_symbol = "[](bold green)";
+        error_symbol = "[](bold red)";
+        vimcmd_symbol = "[](bold green)";
+        vimcmd_replace_one_symbol = "[](bold red)";
+        vimcmd_replace_symbol = "[](bold red)";
+        vimcmd_visual_symbol = "[](bold yellow)";
+      };
+    };
+  };
 }
